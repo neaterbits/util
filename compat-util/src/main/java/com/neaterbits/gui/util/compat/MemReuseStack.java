856b6d1a5cad4ab4632f3e8a09ca78a64a5c33f8
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2016-07-13 19:12:07 +0200

    mem reuse stack

---
+++ compat-util/src/main/java/com/neaterbits/gui/util/compat/MemReuseStack.java
@@ -0,0 +1,65 @@
+package com.neaterbits.gui.util.compat;
+
+import java.util.ArrayList;
+
+public abstract class MemReuseStack<T> {
+	private final ArrayList<T> stack;
+	
+	protected MemReuseStack() {
+		this.stack = new ArrayList<T>();
+	}
+	
+	private int length;
+
+	protected abstract T create();
+	
+	public final T push() {
+		
+		final T ret;
+		
+		final int size = stack.size();
+		
+		if (length < size) {
+			ret = stack.get(length);
+		}
+		else if (length == size) {
+			// Add to stack
+			ret = create();
+			
+			stack.add(ret);
+		}
+		else {
+			// length > size
+			throw new IllegalStateException("length > size");
+		}
+
+		++ length;
+		
+		return ret;
+	}
+	
+	public final T pop() {
+		if (length == 0) {
+			throw new IllegalArgumentException("length == 0");
+		}
+		-- length;
+
+		return stack.get(length);
+	}
+
+	public final T getAt(int idx) {
+		return stack.get(idx);
+	}
+	
+	public final T get() {
+		return stack.get(length - 1);
+	}
+	
+	public final boolean isEmpty() {
+		return length == 0;
+	}
+
+	public final int length() {
+		return length;
+	}
+}
