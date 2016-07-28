d923b25892cad36ee32601ee1af732aee596ed69
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2016-07-28 07:44:22 +0200

    New arrayutils

---
+++ compat-util/src/main/java/com/neaterbits/gui/util/compat/ArrayUtils.java
@@ -0,0 +1,43 @@
+package com.neaterbits.gui.util.compat;
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+
+	public static <T> T[] merge(T [] ... arrays) {
+		
+		if (arrays.length == 0) {
+			throw new IllegalArgumentException("no arrays");
+		}
+		
+		if (arrays.length == 1) {
+			throw new IllegalArgumentException("only one array");
+		}
+		
+		int totalLength = 0;
+		
+		for (T [] ar : arrays) {
+			totalLength += ar.length;
+		}
+		
+		final T [] ret = Arrays.copyOf(arrays[0], totalLength);
+		
+		int pos = arrays[0].length;
+		
+		for (int i = 1; i < arrays.length; ++ i) {
+			
+			final T [] src = arrays[i];
+			
+			System.arraycopy(src, 0, ret, pos, src.length);
+			
+			pos += src.length;
+		}
+		
+		return ret;
+	}
+
+
+	public static <T> T [] subArray(T [] array, int startIdx) {
+		return Arrays.copyOfRange(array, startIdx, array.length);
+	}
+}
---
+++ compat-util/src/test/java/com/neaterbits/gui/util/compat/ArrayUtilsTest.java
@@ -0,0 +1,32 @@
+package com.neaterbits.gui.util.compat;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayUtilsTest {
+
+	@Test(groups={"unit"})
+	public void testMerge() {
+		final Integer [] ret = ArrayUtils.merge(
+				new Integer [] { 9, 8, 7 },
+				new Integer [] { 5, 6 },
+				new Integer [] { 3, 2, 1, 4 });
+		
+		
+		assertThat(ret).isEqualTo(new Integer [] {
+				9, 8, 7, 5, 6, 3, 2, 1, 4
+		});
+	}
+
+	@Test(groups={"unit"})
+	public void testSubArray() {
+		
+		final Integer [] input = new Integer [] { 3, 2, 1 };
+		
+		assertThat(ArrayUtils.subArray(input, 0)).isEqualTo(input);
+		assertThat(ArrayUtils.subArray(input, 1)).isEqualTo(new Integer [] { 2, 1 });
+		assertThat(ArrayUtils.subArray(input, 2)).isEqualTo(new Integer [] { 1 });
+		assertThat(ArrayUtils.subArray(input, 3)).isEqualTo(new Integer [] {  });
+	}
+}
