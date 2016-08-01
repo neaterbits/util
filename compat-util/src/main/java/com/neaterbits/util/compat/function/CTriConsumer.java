1dab22661996c8293bab938df8765f3490d7ece8
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2016-08-01 18:59:23 +0200

    Added CTriConsumer and CTriFunction

---
+++ compat-util/src/main/java/com/neaterbits/util/compat/function/CTriConsumer.java
@@ -0,0 +1,7 @@
+package com.neaterbits.util.compat.function;
+
+public interface CTriConsumer<S, T, U> {
+
+	void accept(S s, T t, U u);
+	
+}
---
+++ compat-util/src/main/java/com/neaterbits/util/compat/function/CTriFunction.java
@@ -0,0 +1,7 @@
+package com.neaterbits.util.compat.function;
+
+public interface CTriFunction<S, T, U, R> {
+
+	R apply(S s, T t, U u);
+	
+}
