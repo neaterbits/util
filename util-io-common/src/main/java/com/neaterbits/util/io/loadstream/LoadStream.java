e3cd57b07ae9896f1938651a7e2dd90737c89f72
Nils Henrik Lorentzen <nils.lorentzen@gmail.com>
2019-05-24 01:33:43 +0200

    Moved loadstreams to util

---
+++ util-io-common/src/main/java/com/neaterbits/util/io/loadstream/LoadStream.java
@@ -0,0 +1,22 @@
+package com.neaterbits.util.io.loadstream;
+
+import java.io.IOException;
+
+/**
+ * A stream that is being loaded from
+ * 
+ * @author nhl
+ *
+ */
+
+public abstract class LoadStream {
+	
+	/**
+	 * Reads data into buffer, may block if waiting for external IO
+	 * @return status from StreamStatus class
+	 */
+	
+	public abstract long read(char [] buffer, int offset, int length) throws IOException;
+	
+	public abstract void close() throws IOException;
+}
